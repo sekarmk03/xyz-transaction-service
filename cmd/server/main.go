ccc5c50d3116a50ff2742e56a11def63b6be2fa4
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-09-22 23:54:53 +0700

    feat: add main server file

---
+++ cmd/server/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"xyz-transaction-service/common/config"
+	gormConn "xyz-transaction-service/common/gorm"
+	commonJwt "xyz-transaction-service/common/jwt"
+	"xyz-transaction-service/common/mysql"
+	"xyz-transaction-service/server"
+
+	"google.golang.org/grpc"
+	"gorm.io/gorm"
+)
+
+func main() {
+	cfg, cerr := config.NewConfig(".env")
+	checkError(cerr)
+
+	splash(cfg)
+
+	dsn, derr := mysql.NewPool(&cfg.MySQL)
+	checkError(derr)
+
+	db, gerr := gormConn.NewMySQLGormDB(dsn)
+	checkError(gerr)
+
+	jwtManager := commonJwt.NewJWT(cfg.JWT.JwtSecretKey, cfg.JWT.TokenDuration)
+
+	grpcServer := server.NewGrpcServer(cfg.Port.GRPC, jwtManager)
+	grpcConn := server.InitGRPCConn(fmt.Sprintf("127.0.0.1:%v", cfg.Port.GRPC), false, "")
+
+	registerGrpcHandlers(grpcServer.Server, *cfg, db, grpcConn)
+
+	_ = grpcServer.Run()
+	_ = grpcServer.AwaitTermination()
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func registerGrpcHandlers(server *grpc.Server, cfg config.Config, db *gorm.DB, grpcConn *grpc.ClientConn) {
+	// consumerModule.InitGrpc(server, cfg, db, grpcConn)
+}
+
+func splash(cfg *config.Config) {
+	colorReset := "\033[0m"
+	colorCyan := "\033[36m"
+
+	fmt.Println(colorCyan, fmt.Sprintf(`-> GRPC xyz-consumer service server started on port :%s`, cfg.Port.GRPC))
+	fmt.Println(colorReset, "")
+}
